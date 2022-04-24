562475ecbbbedde2f56f88fccacfbb3e41aceb84
CamiWallace <59678253+CamiWallace@users.noreply.github.com>
2022-04-23 21:51:53 -0600

    :
    Signed-off-by: CamiWallace <59678253+CamiWallace@users.noreply.github.com>

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connection');
 
-const app = express()
+const app = express();
 
 const port = process.env.PORT || 3000;
 
