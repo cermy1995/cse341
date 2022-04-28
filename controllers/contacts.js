13e1153a5fc191f76b59ba98dc7385ab39feb7f1
CamiWallace <59678253+CamiWallace@users.noreply.github.com>
2022-04-27 21:54:20 -0600

    adding endpoints
    Signed-off-by: CamiWallace <59678253+CamiWallace@users.noreply.github.com>

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -29,7 +29,71 @@ const getSingleContact = async (req, res, next) => {
     });
 };
 
+//function for createing a new contact
+const createContact = async (req, res, next) => {
+    //POST request body
+    const newContact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb
+        .getDb()
+        .db()
+        .collection('contacts')
+        .insertOne(newContact);
+    if (response.acknowldged) {
+        res.status(201).json(response);
+    } else {
+        res.status(500).json(response.error || 'An Error has occurred trying to create contact');
+    }
+};
+
+const updateContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    //Request body
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .replaceOne({ _id: userId }, contact);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    }
+  };
+
+  const deleteContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb
+        .getDb()
+        .db()
+        .collection('contacts')
+        .remove({ _id: userId }, true);
+    console.log(response);
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    }
+  };
+
+
 module.exports = {
     getAllContacts, 
-    getSingleContact
+    getSingleContact,
+    createContact,
+    updateContact,
+    deleteContact
 };
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,8 +3,17 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
+//Get routes
 router.get('/', contactsController.getAllContacts);
-
 router.get('/:id', contactsController.getSingleContact);
+
+//POST routes to create a new contact
+router.post('/', contactsController.createContact);
+
+//PUT routes to update a contact
+router.put('/:id', contactsController.updateContact);
+
+//Delete route for removing a contact
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
